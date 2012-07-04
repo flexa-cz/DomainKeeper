abf600de5e441f2910e46672704b53b8f6279c53
Milan Vlahovic <flexa@flexa.cz>
2012-07-04 10:59:21 +0200

    lang helper, ikony

---
+++ application/language/czech/domain_keeper_lang.php
@@ -0,0 +1,16 @@
+<?php
+// hlavicka tabulky
+$lang['dk_id']='id';
+$lang['dk_domain']='doména';
+$lang['dk_realy_address']='skutečná DNS adresa';
+$lang['dk_tests']='testy';
+$lang['dk_ip']='IP adresa';
+$lang['dk_http_response']='HTTP kód';
+$lang['dk_http_state']='HTTP stav';
+
+// hlaseni testu
+$lang['dk_inactive_item']='v databázi je označeno jako neaktivní';
+$lang['dk_undesirable_request']='nežádoucí odpověď serveru';
+$lang['dk_not_our_ip']='není naše IP adresa';
+$lang['dk_old_ip']='stará IP adresa';
+$lang['dk_server_not_response']='server neodpověděl';
